5ec72665b873fecd6f2e8d4b45fd57f0755d9965
edward <982084060@qq.com>
2025-11-10 18:07:21 +0800

    小财官分支

---
+++ src/test/java/com/yuchang/aiagent/YuAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuchang.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
